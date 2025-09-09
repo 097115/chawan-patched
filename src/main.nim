ff1fa9aa0ea6f25cdacb3d3e5c4a8776b63f5688
097115 <097115@gmail.com>
2025-09-09 04:52:34 +0300

    chore: version updated

--- src/main.nim
+++ src/main.nim
@@ -22,7 +22,7 @@ import utils/sandbox
 import utils/strwidth
 import utils/twtstr
 
-const ChaVersionStr0 = "Chawan browser v0.3"
+const ChaVersionStr0 = "0.3.2025-09-08"
 
 const ChaVersionStr = block:
   var s = ChaVersionStr0 & " ("
